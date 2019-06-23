d1065d146e0a246468ea1a53b648ad9371eb8804
EBayler <erika.bayler@gmail.com>
2019-06-22 23:14:34 -0600

    large update

---
+++ config/orm.js
@@ -0,0 +1,45 @@
+
+ var connection;
+
+ if (process.env.JAWSDB_URL) {
+     var connection = require("../config/connection.js");
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    });
+}
+
+ 
+ var orm = {
+     selectAll: function (tableInput, cb) {
+         var queryString = "SELECT * FROM " + tableInput + ";";
+         connection.query(queryString, function (err, result) {
+             if (err) {
+                 throw err;
+             }
+             cb(result);
+         });
+     },
+     insertOne: function (tableInput, colName, value, cb) {
+         value = connection.escape(value);
+         var queryString = "INSERT INTO " + tableInput + " (" + colName + ") VALUES (" + value + ");";
+         connection.query(queryString, function (err, result) {
+             if (err) throw err;
+             cb(result);
+         });
+     },
+     updateOne: function (tableInput, colName, value, idColName, id, cb) {
+         var queryString = "UPDATE " + tableInput + " SET " + colName + " = " + value + " WHERE " + idColName + " = " + id + ";";
+         connection.query(queryString, function (err, result) {
+             if (err) throw err;
+             cb(result);
+         });
+     }
+ }
+
+
+ module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,40 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES
+    ("She's A Super Leek Burger"),
+    ("Say It Ain't Cilantro Burger (Doesn't come with cilantro… because cilantro is terrible)"),
+    ("Papaya Was a Rolling Stone Burger"),
+    ("We’re Here, We’re Gruyere, Get Used to It Burger"),
+    ("Chile Relleno-You-Didn’t Burger"),
+    ("Open Sesame Burger (served open-faced on a sesame seed bun)"),
+    ("I Know Why the Cajun Burger Sings"),
+    ("Poutine on the Ritz Burger (comes with poutine fries)"),
+    ("The Cauliflower’s Cumin From Inside the House Burger (comes with cauliflower & cumin)"),
+    ("The Child Molester” (Comes with candy!)"),
+    ("I've Created a Muenster Burger"),
+    ("Fried and Prejudice"),
+    ("The Corn Supremacy"),
+    ("New Yolk City"),
+    ("Avocadon't Worry, Be Happy"),
+    ("A Beef-Car Named Desire"),
+    ("Sweet Dreams Are Made of Cheese"),
+    ("Ham-Let"),
+    ("War and Peas"),
+    ("Sir Lanshallot"),
+    ("Olive-R Twist"),
+    ("Bohemian Radishy"),
+    ("Olive and Let Die"),
+    ("Hit Me With Your Best Shallot"),
+    ("Beef-Er Late Than Never"),
+    ("Don't You Four Cheddar Me"),
+    ("Gordan Hansey"),
+    ("Peas and Thank You"),
+    ("Eggers Can't Be Cheesers"),
+    ("If Looks Could Kale"),
+    ("Mission A-Corn-Plished"),
+    ("Baby You Can Chive My Car"),
+    ("Sweet Home Avocado"),
+    ("License To Kale");
+   
+   
